e6e1600879d7c9e7b1c18d00a8dc20c4f4fa83e1
Tejas <tejasthakuryt1308@gmail.com>
2026-03-30 22:03:40 +0530

    Disable pull-to-refresh functionality in usePullToRefresh
    Removed pulling state and touch event handlers to disable pull-to-refresh functionality.

--- frontend/src/hooks/usePullToRefresh.js
+++ frontend/src/hooks/usePullToRefresh.js
@@ -1,47 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
-export const usePullToRefresh = (onRefresh) => {
-  const [pulling, setPulling] = useState(false);
-
+export const usePullToRefresh = () => {
   useEffect(() => {
-    let startY = 0;
+    // Disabled to prevent mobile scroll bugs
+    // and unwanted refresh issues in production
+  }, []);
 
-    const handleTouchStart = (e) => {
-      if (window.scrollY === 0) {
-        startY = e.touches[0].clientY;
-      }
-    };
-
-    const handleTouchMove = (e) => {
-      if (window.scrollY === 0) {
-        const currentY = e.touches[0].clientY;
-        const diff = currentY - startY;
-        
-        if (diff > 80) {
-          setPulling(true);
-        }
-      }
-    };
-
-    const handleTouchEnd = async () => {
-      if (pulling) {
-        setPulling(false);
-        if (onRefresh) {
-          await onRefresh();
-        }
-      }
-    };
-
-    window.addEventListener('touchstart', handleTouchStart);
-    window.addEventListener('touchmove', handleTouchMove);
-    window.addEventListener('touchend', handleTouchEnd);
-
-    return () => {
-      window.removeEventListener('touchstart', handleTouchStart);
-      window.removeEventListener('touchmove', handleTouchMove);
-      window.removeEventListener('touchend', handleTouchEnd);
-    };
-  }, [pulling, onRefresh]);
-
-  return pulling;
+  return false;
 };
