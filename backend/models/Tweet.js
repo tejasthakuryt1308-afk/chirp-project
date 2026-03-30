b97ec1b8f395d259ce9f15368fa08ca3fa929abf
Tejas <tejasthakuryt1308@gmail.com>
2026-03-30 19:36:54 +0530

    Refactor Tweet schema to include additional fields

--- backend/models/Tweet.js
+++ backend/models/Tweet.js
@@ -1,27 +1,52 @@
 const mongoose = require('mongoose');
 
-const imageSchema = new mongoose.Schema({
-  url: String,
-  width: Number,
-  height: Number
-}, { _id: false });
+const TweetSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+    maxlength: 280
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  images: [{
+    url: String,
+    width: Number,
+    height: Number
+  }],
+  category: {
+    type: String,
+    enum: ['general', 'sports', 'technology', 'business', 'entertainment', 'health', 'science'],
+    default: 'general'
+  },
+  likes: [{
+    type: String // Can be user ID or 'fake-user-xxx' for news
+  }],
+  retweets: [{
+    type: String
+  }],
+  replies: [{
+    user: String,
+    text: String,
+    createdAt: Date
+  }],
+  isNewsArticle: {
+    type: Boolean,
+    default: false
+  },
+  newsSource: String,
+  newsHandle: String,
+  newsLogo: String,
+  articleUrl: String,
+  verified: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
 
-const tweetSchema = new mongoose.Schema({
-  text: { type: String, maxlength: 280, trim: true, default: '' },
-  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, index: true },
-  images: [imageSchema],
-  category: { type: String, default: 'general', index: true },
-  hashtags: [{ type: String, index: true }],
-  mentions: [String],
-  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  retweets: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  replies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tweet' }],
-  replyTo: { type: mongoose.Schema.Types.ObjectId, ref: 'Tweet', default: null, index: true },
-  isNewsArticle: { type: Boolean, default: false, index: true },
-  newsSource: { type: String, default: '' },
-  articleUrl: { type: String, default: '' }
-}, { timestamps: true });
-
-tweetSchema.index({ text: 'text', hashtags: 'text', mentions: 'text' });
-
-module.exports = mongoose.model('Tweet', tweetSchema);
+module.exports = mongoose.model('Tweet', TweetSchema);
